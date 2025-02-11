403bfaf8d170582baa42baeb9db08e49191c1839
serafino16 <40893656+serafino16@users.noreply.github.com>
2025-02-11 11:09:34 +0200

    Add files via upload

---
+++ Spree-Devops/Terraform/lb-sg.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "lb_sec_group" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lb-sec-group"
+  }
+}
+
+resource "aws_security_group" "worker_sec_group" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "worker-sec-group"
+  }
+}
