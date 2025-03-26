a5f084c3d039dae89b569f166858f487ead912f2
Serafino Stoimenov <40893656+serafino16@users.noreply.github.com>
2025-03-26 09:50:52 +0200

    Add files via upload

---
+++ IAM-ADMIN/iam-group-and-user-eksadmins.tf
@@ -0,0 +1,37 @@
+
+resource "aws_iam_group" "eksadmins_iam_group" {
+  name = "${local.name}-eksadmins"
+  path = "/"
+}
+
+
+
+resource "aws_iam_group_policy" "eksadmins_iam_group_assumerole_policy" {
+  name  = "${local.name}-eksadmins-group-policy"
+  group = aws_iam_group.eksadmins_iam_group.name
+
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+        ]
+        Effect   = "Allow"
+        Sid    = "AllowAssumeOrganizationAccountRole"
+        Resource = "${aws_iam_role.eks_admin_role.arn}"
+      },
+    ]
+  })
+}
+
+
+
+resource "aws_iam_group_membership" "eksadmins" {
+  name = "${local.name}-eksadmins-group-membership"
+  users = [
+    aws_iam_user.eksadmin_user.name
+  ]
+  group = aws_iam_group.eksadmins_iam_group.name
+}
